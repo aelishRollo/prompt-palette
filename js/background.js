7b3747f3d1855da1d40869f8094bf0c49e92cec2
Alec Rollison <88508028+aelishRollo@users.noreply.github.com>
2024-06-24 17:11:19 -0400

    Complete ticket 1

--- js/background.js
+++ js/background.js
@@ -1,14 +1,18 @@
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.contextMenus.create({
-      id: "promptPalette",
-      title: "Prompt Palette",
-      contexts: ["all"]
+  chrome.contextMenus.create({
+    id: "promptPalette",
+    title: "Prompt Palette",
+    contexts: ["all"]
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "promptPalette") {
+    chrome.windows.create({
+      url: chrome.runtime.getURL("popup.html"),
+      type: "popup",
+      width: 400,
+      height: 600
     });
-  });
-  
-  chrome.contextMenus.onClicked.addListener((info, tab) => {
-    if (info.menuItemId === "promptPalette") {
-      chrome.action.openPopup();
-    }
-  });
-  
+  }
+});
