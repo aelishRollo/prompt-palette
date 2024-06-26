3dddd1e456d55568060aea7e9603375bccb45dfc
Alec Rollison <88508028+aelishRollo@users.noreply.github.com>
2024-06-26 11:32:01 -0400

    Add default keyboard shortcut to activate extension

--- js/background.js
+++ js/background.js
@@ -1,18 +1,29 @@
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
-    id: "promptPalette",
-    title: "Prompt Palette",
-    contexts: ["page", "selection", "link"]
+      id: "promptPalette",
+      title: "Prompt Palette",
+      contexts: ["all"]
   });
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === "promptPalette") {
-    chrome.windows.create({
-      url: chrome.runtime.getURL("popup.html"),
-      type: "popup",
-      width: 400,
-      height: 600
-    });
+      openPopup();
   }
 });
+
+chrome.commands.onCommand.addListener((command) => {
+  if (command === "open_prompt_palette") {
+      openPopup();
+  }
+});
+
+function openPopup() {
+  chrome.windows.create({
+      url: chrome.runtime.getURL("popup.html"),
+      type: 'popup',
+      focused: true,
+      width: 400,
+      height: 600
+  });
+}
