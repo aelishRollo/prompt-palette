4b23aad0c6d1f3640ab13361573fefc942e4080c
Michael Kochell <6913320+mickmister@users.noreply.github.com>
2025-02-17 15:52:35 -0500

    add typescript support

---
+++ src/background.ts
@@ -0,0 +1,29 @@
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+        id: "promptPalette",
+        title: "Prompt Palette",
+        contexts: ["all"]
+    });
+  });
+
+  chrome.contextMenus.onClicked.addListener((info, tab) => {
+    if (info.menuItemId === "promptPalette") {
+        openPopup();
+    }
+  });
+
+  chrome.commands.onCommand.addListener((command) => {
+    if (command === "open_prompt_palette") {
+        openPopup();
+    }
+  });
+
+  function openPopup() {
+    chrome.windows.create({
+        url: chrome.runtime.getURL("popup.html"),
+        type: 'popup',
+        focused: true,
+        width: 400,
+        height: 600
+    });
+  }
